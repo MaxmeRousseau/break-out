992de201d95527b8c8f3f7a2eb5f6052c16458e3
Maxime Rousseau <maxime.rousseau@etudiant.univ-lr.fr>
2024-05-03 11:34:33 +0200

    Init Commit Branch

---
+++ Paddle.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file Paddle.cpp
+ * @brief
+ *
+ * Created by Dexea 2024 Paddle.cpp
+ */
+
+#include "Paddle.h"
---
+++ Paddle.h
@@ -0,0 +1,25 @@
+/**
+ * @file Paddle.h
+ * @brief
+ *
+ * Created by Dexea 2024 Paddle.h
+ */
+
+#ifndef BREAK_OUT_2024_PADDLE_H
+#define BREAK_OUT_2024_PADDLE_H
+
+#include <iostream>
+#include <QPainter>
+#include <QRect>
+
+using namespace std;
+
+
+class Paddle {
+private:
+    QRect itsPaddle;
+    int itsSpeedX;
+};
+
+
+#endif //BREAK_OUT_2024_PADDLE_H
